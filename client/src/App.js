77abfce675c820bf93904498ee352527f85e585f
Deven Gangwani <devengangwani12@gmail.com>
2023-03-22 01:01:10 +0530

    Changed doctor appointments page

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,43 @@
-import './App.css'
-import Homepage from "./components/homepage/homepage"
-import {Login,global} from "./components/login/login"
-import Register from "./components/register/register"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom" ;
 import { useState } from 'react';
+
+import './App.css'
+
+import Homepage from "./components/homepage/homepage"
+
+import {Login,global} from "./components/login/login"
+
+import Register from "./components/register/register"
+
+import Navbar from './components/Navbar';
+
 import Doctor from './components/doctor/Entry_page';
 import Schedule from './components/doctor/Schedule';
 import Veiwappointments from './components/doctor/Veiw Appointments/veiw_appointments';
-import InitialNurse from "./components/Nurse/InitialNurse"
-import Vitals from "./components/Nurse/Vitals"
+import Conductappointment from './components/doctor/Veiw Appointments/Conduct_appointment';
+
+import InitialNurse from "./components/Nurse/InitialNurse";
+import Vitals from "./components/Nurse/Vitals";
+
 import InitialPharmacy from "./components/Pharmacy/InitialPharmacy"
 import Prescription from "./components/Pharmacy/Prescription"
-import CompletedPrescription from "./components/Pharmacy/CompletedPrescription"
-import Navbar from './components/Navbar';
+import CompletedPrescription from "./components/Pharmacy/CompletedPrescription";
+
 import StudentHome from "./components/Student/Home";
 import StudentRequest from "./components/Student/Request";
 import StudentUpcoming from "./components/Student/Upcoming";
 import StudentHistory from "./components/Student/History";
 import StudentDoctors from "./components/Student/Doctors";
+
 import InitialReceptionist from './components/Receptionist/InitialReceptionist';
 import AppointmentsRequests from './components/Receptionist/AppointmentsRequests';
 import DoctorAllotment from './components/Receptionist/DoctorAllotment';
-
-import Conductappointment from './components/doctor/Veiw Appointments/Conduct_appointment';
-
 import DoctorAppointments from './components/Receptionist/DoctorAppointments';
+import UploadReports from './components/Receptionist/UploadReports';
+import AppointmentStatus from './components/Receptionist/AppointmentStatus';
 
 import pendingAppointments from './Data';
 import { ongoingAppointments, completedAppointments } from './Data';
-import UploadReports from './components/Receptionist/UploadReports';
-import AppointmentStatus from './components/Receptionist/AppointmentStatus';
 
 function App() {
 
@@ -53,26 +61,32 @@ function App() {
             }
           </Route>
           <Route path="/login"><Login setLoginUser={setLoginUser}/></Route>
+          
           <Route path="/register"><Register /></Route>
+          
           <Route exact path="/doctor"><Doctor/></Route>
           <Route exact path="/doctor/schedule"><Schedule/></Route>
           <Route exact path="/doctor/view_appointment"><Veiwappointments/></Route>
+          <Route exact path="/doctor/conduct_appointment"> <Conductappointment/> </Route>
+          
           <Route exact path="/nurse/"><InitialNurse name="abcd" id={420}/></Route>
           <Route exact path="/nurse/vitals"><Vitals/></Route>
+          
           <Route exact path="/pharmacy/"><InitialPharmacy prescriptions={ongoingAppointments}/></Route>
           <Route exact path="/pharmacy/prescription"><Prescription/></Route>
           <Route exact path="/pharmacy/completedprescription"><CompletedPrescription prescriptions={completedAppointments}/></Route>
+          
           <Route exact path="/student"> <StudentHome /></Route>
           <Route exact path="/student/request"><StudentRequest /></Route>
           <Route exact path="/student/upcoming"><StudentUpcoming /></Route>
           <Route exact path="/student/history"><StudentHistory /></Route>
           <Route exact path="/student/doctors"><StudentDoctors /></Route>
+          
           <Route exact path="/receptionist/"><InitialReceptionist/></Route>
           <Route exact path="/receptionist/"><InitialReceptionist/></Route>
           <Route exact path="/receptionist/appointments"><AppointmentsRequests prescriptions={pendingAppointments}/></Route>
           <Route exact path="/receptionist/appointmentstatus"><AppointmentStatus prescriptions={ongoingAppointments}/></Route>
           <Route exact path="/receptionist/doctorallotment"><DoctorAllotment/></Route>
-          <Route exact path="/doctor/conduct_appointment"> <Conductappointment/> </Route>
           <Route exact path="/receptionist/setschedule"><DoctorAppointments/></Route>
           <Route exact path="/receptionist/uploadreports"><UploadReports/></Route>
         </Switch>
--- client/src/components/doctor/Veiw Appointments/Conduct_appointment.js
+++ client/src/components/doctor/Veiw Appointments/Conduct_appointment.js
@@ -1,11 +1,19 @@
 import React,{useState} from 'react'
 import axios from 'axios'
 import {useHistory } from 'react-router-dom'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+
+
+
 const Conduct_appointment = () => {
     const history = useHistory();
     const [medicine, setMedicine] = useState([]);
     // var days = 0;
 
+	const [tab, setTab] = useState(0);
+    
     const handleMinus = () => {
         var input = document.getElementById('days');
         if(input.value > 0) input.value = parseInt(input.value) - 1;
@@ -51,78 +59,111 @@ const Conduct_appointment = () => {
         document.getElementById('days').value = 0;
     }
 
+	const handleTabs = (event, value) => {
+		setTab(value);
+	}
+
+	const Prescribe = () => (
+	<>
+		<h2 className="mb-5">Medication</h2>
+			<div className="container my-3">
+				<div className="input-group mb-3">
+					<span className="input-group-text" id="inputGroup-sizing-default">Medicine Name</span>
+					<input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="name"/>
+				</div>
+			</div>
+			<div className="btn-group my-3" role="group" aria-label="Basic radio toggle button group">  
+				<input type="radio" className="btn-check" name="btnradio" id="btnradio1" autocomplete="off" value="qD"/>
+				<label className="btn btn-outline-primary" htmlFor="btnradio1">qD</label>
+
+				<input type="radio" className="btn-check" name="btnradio" id="btnradio2" autocomplete="off" value="BID"/>
+				<label className="btn btn-outline-primary" htmlFor="btnradio2">BID</label>
+
+				<input type="radio" className="btn-check" name="btnradio" id="btnradio3" autocomplete="off" value="TID"/>
+				<label className="btn btn-outline-primary" htmlFor="btnradio3">TID</label>
+
+				<input type="radio" className="btn-check" name="btnradio" id="btnradio4" autocomplete="off" value="BBF"/>
+				<label className="btn btn-outline-primary" htmlFor="btnradio4">BBF</label>
+
+				<input type="radio" className="btn-check" name="btnradio" id="btnradio5" autocomplete="off" value="BD"/>
+				<label className="btn btn-outline-primary" htmlFor="btnradio5">BD</label>
+			</div>
+
+			<div className="row my-3">
+				<div className="col-5 align-center">
+					Days:
+				</div>
+				<div className="col-5">
+					<div className="input-group mb-3">
+						<button onClick={handleMinus} className="btn btn-dark" type="button" id="button-addon2">-</button>
+						<input id="days" style={{textAlign: "center"}} type="number" className="form-control" placeholder="0" aria-label="Number of Days" aria-describedby="button-addon2" value={0} readOnly/>
+						<button onClick={handlePlus} className="btn btn-dark" type="button" id="button-addon2">+</button>
+					</div>
+				</div>
+			</div>
+
+			<button className="btn btn-dark my-3" onClick={handleClick}>Add Medicine</button>
+			<div className="row my-3">
+				<div className="col-6">
+					<h4>Current Prescription</h4>
+				</div>
+				<div className="col-6">
+					<h4 className="justify-content-center">Remarks</h4>
+				</div>
+				<div className="col-6">
+					{
+						medicine?.map((element) => {
+							return(
+								<div className="row">
+									{element.name} {element.dose}-{element.days}
+								</div>
+							);
+						})
+					}
+				</div>
+				<div className="col-6">
+					<div className="input-group">
+						{/* <span className="input-group-text">With textarea</span> */}
+						<textarea value={val} onChange={changed}  className="form-control" rows="6" aria-label="With textarea"></textarea>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+
+	const MedicalHistory = () => (
+		<>
+			<h1>WIP</h1>
+		</>
+	);
+	
+	const DiagnosticTests = () => (
+		<>
+			<h1>WIP</h1>
+		</>
+	);
+
     return (
-        <div className="container row">
-            <div className="col-5" role="group" aria-label="Vertical button group">
-                <button type="button" className="btn btn-dark col-12 my-5" onClick={main}>Prescribe Medication</button>
-                <button type="button" className="btn btn-dark col-12 my-5">Medical History</button>
-                <button type="button" className="btn btn-dark col-12 my-5">Specialist Referral</button>
-                <button type="button" className="btn btn-dark col-12 my-5">Diagnostic Tests</button>
+        <div className="container h-100">
+        	<div className="row">
+        	<div className="col-2">
+            <Tabs value={tab} onChange={handleTabs} orientation="vertical">
+				<Tab label="Prescribe Medication" value={0}/>
+				<Tab label="Medical History" value={1}/>
+				<Tab label="Diagnostic Tests" value={2}/>
+			</Tabs>
+			</div>
+            <div className="col-10 text-center col">
+                {
+                	tab === 0
+                	? <Prescribe />
+                	: tab === 1
+                	? <MedicalHistory />
+                	: tab === 2
+                	? <DiagnosticTests />
+                	: ""
+                }
             </div>
-            <div className="col-7 text-center">
-                <h2 className="mb-5">Medication</h2>
-                <div className="container my-3">
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="inputGroup-sizing-default">Medicine Name</span>
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="name"/>
-                    </div>
-                </div>
-                <div class="btn-group my-3" role="group" aria-label="Basic radio toggle button group">  
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" value="qD"/>
-                    <label class="btn btn-outline-primary" htmlFor="btnradio1">qD</label>
-
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" value="BID"/>
-                    <label class="btn btn-outline-primary" htmlFor="btnradio2">BID</label>
-
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" value="TID"/>
-                    <label class="btn btn-outline-primary" htmlFor="btnradio3">TID</label>
-
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off" value="BBF"/>
-                    <label class="btn btn-outline-primary" htmlFor="btnradio4">BBF</label>
-
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off" value="BD"/>
-                    <label class="btn btn-outline-primary" htmlFor="btnradio5">BD</label>
-                </div>
-
-                <div className="row my-3">
-                    <div className="col-5 align-center">
-                        Days:
-                    </div>
-                    <div className="col-5">
-                        <div class="input-group mb-3">
-                            <button onClick={handleMinus} class="btn btn-dark" type="button" id="button-addon2">-</button>
-                            <input id="days" style={{textAlign: "center"}} type="number" class="form-control" placeholder="0" aria-label="Number of Days" aria-describedby="button-addon2" value={0} readOnly/>
-                            <button onClick={handlePlus} class="btn btn-dark" type="button" id="button-addon2">+</button>
-                        </div>
-                    </div>
-                </div>
-
-                <button className="btn btn-dark my-3" onClick={handleClick}>Add Medicine</button>
-                <div className="row my-3">
-                    <div className="col-6">
-                        <h4>Current Prescription</h4>
-                    </div>
-                    <div className="col-6">
-                        <h4 className="justify-content-center">Remarks</h4>
-                    </div>
-                    <div className="col-6">
-                        {
-                            medicine?.map((element) => {
-                                return(
-                                    <div className="row">
-                                        {element.name} {element.dose}-{element.days}
-                                    </div>
-                                );
-                            })
-                        }
-                    </div>
-                    <div className="col-6">
-                        <div class="input-group">
-                            {/* <span class="input-group-text">With textarea</span> */}
-                            <textarea value={val} onChange={changed}  class="form-control" rows="6" aria-label="With textarea"></textarea>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     );
